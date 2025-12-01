5f3c95b2540f6d028a7979ce718d210406d39c8a
kbzaman76 <kbzaman320@gmail.com>
2025-12-01 21:26:25 +0600

    SEO added

--- timocore/resources/views/templates/basic/user/auth/register.blade.php
+++ timocore/resources/views/templates/basic/user/auth/register.blade.php
@@ -245,3 +245,29 @@
         }
     </style>
 @endpush
+@push('seo')
+    <meta name="description"
+        content="TimoDesk is the best employee time tracking software app. You can remotely track and analyze the performance of your team of any size and location.">
+
+    <link rel="shortcut icon" href="https://timodesk.com/assets/images/logo/favicon.png" type="image/x-icon">
+    <link rel="canonical" href="https://dash.timodesk.com/register">
+
+
+    <link rel="apple-touch-icon" href="https://timodesk.com/assets/images/logo/logo.png">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title" content="Register | TimoDesk - Time Tracking Software For Global Teams">
+
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Register | TimoDesk - Time Tracking Software For Global Teams">
+    <meta property="og:description"
+        content="TimoDesk is the best employee time tracking software app. You can remotely track and analyze the performance of your team of any size and location.">
+    <meta property="og:image" content="https://timodesk.com/assets/images/seo.png">
+
+    <meta property="og:image:type" content="image/png">
+    <meta property="og:image:width" content="1180">
+    <meta property="og:image:height" content="600">
+    <meta property="og:url" content="https://dash.timodesk.com/register">
+
+    <meta name="twitter:card" content="summary_large_image">
+@endpush
